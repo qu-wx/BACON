cb51693c40ac7e83eb01f4f4746e4e23410b4fb1
qu-wx <quwenxin@zju.edu.cn>
2024-10-17 10:25:28 +0800

    BACON

--- R/BACON_visual.R
+++ R/BACON_visual.R
@@ -1,3 +1,178 @@
+#' Generate color from a customed color palette
+#'
+#' @param n number of color
+#' @importFrom grDevices colorRampPalette
+#' @return
+#' @export
+#'
+#' @examples
+scPalette <- function(n) {
+  colorSpace <- c('#E41A1C','#377EB8','#4DAF4A','#984EA3','#F29403','#F781BF','#BC9DCC','#A65628','#54B0E4','#222F75','#1B9E77','#B2DF8A',
+                  '#E3BE00','#FB9A99','#E7298A','#910241','#00CDD1','#A6CEE3','#CE1261','#5E4FA2','#8CA77B','#00441B','#DEDC00','#B3DE69','#8DD3C7','#999999')
+  if (n <= length(colorSpace)) {
+    colors <- colorSpace[1:n]
+  } else {
+    colors <- grDevices::colorRampPalette(colorSpace)(n)
+  }
+  return(colors)
+}
+#' Chord diagram for visualizing QS communication from a weighted adjacency matrix
+#'
+#' @param net weighted matrix with columns defining the QS network
+#' @param color.use color use for groups
+#' @param group group labels the sector name should be used as the names in the vector
+#' @param cell.order defining the bacteria type orders
+#' @param sources.use vector giving the index or the name of source bacterial groups
+#' @param targets.use vector giving the index or the name of target bacterial groups
+#' @param lab.cex font size for the text
+#' @param small.gap small gap between sectors
+#' @param big.gap Gap between the different sets of sectors, which are defined in the `group` parameter
+#' @param annotationTrackHeight annotationTrack height
+#' @param remove.isolate whether remove sectors without any links
+#' @param link.visible whether plot the link. The value is logical, if it is set to FALSE, the corresponding link will not plotted, but the space is still ocuppied.
+#' @param scale scale each sector to same width
+#' @param directional Whether links have directions. 1 means the direction is from the first column in df to the second column, -1 is the reverse, 0 is no direction, and 2 for two directional.
+#' @param link.target.prop If the Chord diagram is directional, for each source sector, whether to draw bars that shows the proportion of target sectors
+#' @param reduce if the ratio of the width of certain grid compared to the whole circle is less than this value, the grid is removed on the plot. Set it to value less than zero if you want to keep all tiny grid.
+#' @param transparency transparency of link colors
+#' @param link.border border for links, single vector or a matrix with names or a data frame with source, target , prob, three column information
+#' @param title.name title name of the plot
+#' @param show.legend whether show the figure legend
+#' @param legend.pos.x,legend.pos.y adjust the legend position
+#' @importFrom circlize circos.clear chordDiagram circos.track circos.text get.cell.meta.data
+#' @importFrom grDevices recordPlot colorRampPalette
+#' @return
+#' @export
+#'
+#' @examples
+netVisual_chord_cell_internal <- function(net, color.use = NULL, group = NULL, cell.order = NULL,
+                                          sources.use = NULL, targets.use = NULL,
+                                          lab.cex = 0.8,small.gap = 1, big.gap = 10, annotationTrackHeight = c(0.03),
+                                          remove.isolate = FALSE, link.visible = TRUE, scale = FALSE, directional = 1, link.target.prop = TRUE, reduce = -1,
+                                          transparency = 0.4, link.border = NA,
+                                          title.name = NULL, show.legend = FALSE, legend.pos.x = 20, legend.pos.y = 20,...){
+  if (inherits(x = net, what = c("matrix", "Matrix"))) {
+    cell.levels <- union(rownames(net), colnames(net))
+    net <- reshape2::melt(net, value.name = "prob")
+    colnames(net)[1:2] <- c("source","target")
+  } else if (is.data.frame(net)) {
+    if (all(c("source","target", "prob") %in% colnames(net)) == FALSE) {
+      stop("The input data frame must contain three columns named as source, target, prob")
+    }
+    cell.levels <- as.character(union(net$source,net$target))
+  }
+  if (!is.null(cell.order)) {
+    cell.levels <- cell.order
+  }
+  net$source <- as.character(net$source)
+  net$target <- as.character(net$target)
+
+  # keep the interactions associated with sources and targets of interest
+  if (!is.null(sources.use)){
+    if (is.numeric(sources.use)) {
+      sources.use <- cell.levels[sources.use]
+    }
+    net <- subset(net, source %in% sources.use)
+  }
+  if (!is.null(targets.use)){
+    if (is.numeric(targets.use)) {
+      targets.use <- cell.levels[targets.use]
+    }
+    net <- subset(net, target %in% targets.use)
+  }
+  # remove the interactions with zero values
+  net <- subset(net, prob > 0)
+  if(dim(net)[1]<=0){message("No interaction between those cells")}
+  # create a fake data if keeping the cell types (i.e., sectors) without any interactions
+  if (!remove.isolate) {
+    cells.removed <- setdiff(cell.levels, as.character(union(net$source,net$target)))
+    if (length(cells.removed) > 0) {
+      net.fake <- data.frame(cells.removed, cells.removed, 1e-10*sample(length(cells.removed), length(cells.removed)))
+      colnames(net.fake) <- colnames(net)
+      net <- rbind(net, net.fake)
+      link.visible <- net[, 1:2]
+      link.visible$plot <- FALSE
+      if(nrow(net) > nrow(net.fake)){
+        link.visible$plot[1:(nrow(net) - nrow(net.fake))] <- TRUE
+      }
+      scale = TRUE
+    }
+  }
+
+  df <- net
+  cells.use <- union(df$source,df$target)
+
+  # define grid order
+  order.sector <- cell.levels[cell.levels %in% cells.use]
+
+  # define grid color
+  if (is.null(color.use)){
+    color.use = scPalette(length(cell.levels))
+    names(color.use) <- cell.levels
+  } else if (is.null(names(color.use))) {
+    names(color.use) <- cell.levels
+  }
+  grid.col <- color.use[order.sector]
+  names(grid.col) <- order.sector
+
+  # set grouping information
+  if (!is.null(group)) {
+    group <- group[names(group) %in% order.sector]
+  }
+
+  # define edge color
+  edge.color <- color.use[as.character(df$source)]
+
+  if (directional == 0 | directional == 2) {
+    link.arr.type = "triangle"
+  } else {
+    link.arr.type = "big.arrow"
+  }
+
+  circos.clear()
+  chordDiagram(df,
+               order = order.sector,
+               col = edge.color,
+               grid.col = grid.col,
+               transparency = transparency,
+               link.border = link.border,
+               directional = directional,
+               direction.type = c("diffHeight","arrows"),
+               link.arr.type = link.arr.type, # link.border = "white",
+               annotationTrack = "grid",
+               annotationTrackHeight = annotationTrackHeight,
+               preAllocateTracks = list(track.height = max(strwidth(order.sector))),
+               small.gap = small.gap,
+               big.gap = big.gap,
+               link.visible = link.visible,
+               scale = scale,
+               group = group,
+               link.target.prop = link.target.prop,
+               reduce = reduce,
+               ...)
+  circos.track(track.index = 1, panel.fun = function(x, y) {
+    xlim = get.cell.meta.data("xlim")
+    xplot = get.cell.meta.data("xplot")
+    ylim = get.cell.meta.data("ylim")
+    sector.name = get.cell.meta.data("sector.index")
+    circos.text(mean(xlim), ylim[1], sector.name, facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5),cex = lab.cex)
+  }, bg.border = NA)
+
+  # https://jokergoo.github.io/circlize_book/book/legends.html
+  if (show.legend) {
+    lgd <- ComplexHeatmap::Legend(at = names(grid.col), type = "grid", legend_gp = grid::gpar(fill = grid.col), title = "Cell State")
+    ComplexHeatmap::draw(lgd, x = unit(1, "npc")-unit(legend.pos.x, "mm"), y = unit(legend.pos.y, "mm"), just = c("right", "bottom"))
+  }
+
+  if(!is.null(title.name)){
+    # title(title.name, cex = 1)
+    text(-0, 1.02, title.name, cex=1)
+  }
+  circos.clear()
+  gg <- recordPlot()
+  return(gg)
+}
+
 #' Chord diagram of QS communication for single or multiple communication pairs
 #'
 #' @param object a BACON object
@@ -5,7 +180,8 @@
 #' @param synthesis_subpopulation synthesis group used for plot
 #' @param reception_subpopulation reception group used for plot
 #' @param lab.cex
-#' @importFrom CellChat netVisual_chord_cell_internal
+#' @importFrom circlize circos.clear chordDiagram circos.track circos.text get.cell.meta.data
+#' @importFrom dplyr select %>% group_by summarize
 #' @return
 #' @export
 #'
@@ -16,7 +192,7 @@ netVisual_chord_BACON <- function(object, interaction_use='all', synthesis_subpo
   if(interaction_use=='all') {
     net_aggregated <- net_aggregation(object@net)}
   else {net_aggregated <- net_aggregation(object@net[interaction_use])}
-  CellChat::netVisual_chord_cell_internal(net_aggregated,lab.cex=lab.cex)
+  netVisual_chord_cell_internal(net_aggregated,lab.cex=lab.cex)
 }
 
 #' Circle plot for QS communication network
@@ -47,8 +223,8 @@ netVisual_chord_BACON <- function(object, interaction_use='all', synthesis_subpo
 #' @return
 #' @export
 #' @importFrom igraph graph_from_adjacency_matrix ends E V layout_ in_circle
-#' @importFrom grDevices recordPlot
-#' @import CellChat
+#' @importFrom grDevices recordPlot colorRampPalette
+#' @import grDevices
 #' @examples
 netVisual_circle_BACON <-function(net_ori, color.use = NULL,title.name = NULL, synthesis_sub = NULL, reception_sub = NULL, remove.isolate = FALSE, top = 1,
                                   weight.scale = TRUE, vertex.weight = 1, vertex.weight.max = NULL, vertex.size.max = NULL, vertex.label.cex=1.2,vertex.label.color= "black",
@@ -119,14 +295,14 @@ netVisual_circle_BACON <-function(net_ori, color.use = NULL,title.name = NULL, s
     coords_scale<-coords
   }
   if (is.null(color.use)) {
-    color.use = CellChat::scPalette(length(igraph::V(g)))
+    color.use = scPalette(length(igraph::V(g)))
   }
   if (is.null(vertex.weight.max)) {
     vertex.weight.max <- max(vertex.weight)
   }
   vertex.weight <- vertex.weight/vertex.weight.max*vertex.size.max+5
   # color.use.label  <- c('red','black')
-  color.use.label <- c('black',CellChat::scPalette(length(unique(group))))[1:length(unique(group))]
+  color.use.label <- c('black',scPalette(length(unique(group))))[1:length(unique(group))]
   color.use.label <- structure(color.use.label,names=unique(group))
   # color.use.label <- color.use.label[-length(color.use.label)]
   # if(length(color.use.label)==1){ color.use.label='black'}
